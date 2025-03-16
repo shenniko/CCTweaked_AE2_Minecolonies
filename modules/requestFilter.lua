f6af3db795d398d14a5631f564824628c971964f
shenniko <30448631+shenniko@users.noreply.github.com>
2025-03-16 14:05:48 +0000

    Update requestFilter.lua
    update list

--- modules/requestFilter.lua
+++ modules/requestFilter.lua
@@ -1,28 +1,30 @@
 -- Version: 1.0
--- requestFilter.lua
--- Determines whether a colony request should be skipped
+-- requestFilter.lua - Filters item names or descriptions that should not use ME
 
-local filter = {}
+local requestFilter = {}
 
--- Add terms/types you want to skip here
-local blockedKeywords = {
-    "Tool of class",
-    "Hoe", "Shovel", "Pickaxe", "Axe", "Sword", "Helmet", "Boots", "Chestplate", "Leggings",
-    "Compostable", "Food", "Fuel", "Flower", "Ore"
+-- List of words that imply we shouldn't use ME to fulfill
+local skipKeywords = {
+    "Food", "Fuel", "Flowers", "Compostable", "Smeltable Ore", "Stack List"
 }
 
--- Check if a request should be skipped based on name or description
-function filter.shouldSkip(request)
-    local name = request.name:lower()
-    local desc = request.desc:lower()
+-- List of common equipment keywords
+local equipmentKeywords = {
+    "Hoe", "Axe", "Pickaxe", "Shovel", "Sword", "Helmet", "Chestplate", "Leggings", "Boots", "Bow", "Shield"
+}
 
-    for _, keyword in ipairs(blockedKeywords) do
-        if name:find(keyword:lower()) or desc:find(keyword:lower()) then
-            return true
-        end
+function requestFilter.shouldSkipItem(name)
+    for _, word in ipairs(skipKeywords) do
+        if name:find(word) then return true end
     end
-
     return false
 end
 
-return filter
+function requestFilter.isEquipment(name)
+    for _, word in ipairs(equipmentKeywords) do
+        if name:find(word) then return true end
+    end
+    return false
+end
+
+return requestFilter
